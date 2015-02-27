c3f9b0edc036bd87eb20a2c7951d5c2efd6be4a2
Ilyas Abbas <Ily4s@users.noreply.github.com>
2015-02-27 15:19:45 +0100

    Update Softether_Setup-x64.sh

--- Softether_Setup-x64.sh
+++ Softether_Setup-x64.sh
@@ -5,10 +5,10 @@ apt-get upgrade -y
 apt-get install curl -y
 apt-get install build-essential -y
 #Download SoftEther
-wget http://www.softether-download.com/files/softether/v4.12-9514-beta-2014.11.17-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.12-9514-beta-2014.11.17-linux-x64-64bit.tar.gz
+wget http://www.softether-download.com/files/softether/v4.14-9529-beta-2015.02.02-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.14-9529-beta-2015.02.02-linux-x64-64bit.tar.gz
 sleep 5
 #Extract SoftEther
-tar -xzvf softether-vpnserver-v4.12-9514-beta-2014.11.17-linux-x64-64bit.tar.gz
+tar -xzvf softether-vpnserver-v4.14-9529-beta-2015.02.02-linux-x64-64bit.tar.gz
 sleep 5
 #Compile SoftEther
 cd vpnserver
